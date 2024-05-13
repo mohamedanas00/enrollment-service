13b1c296c56bb6bcf37b090a6aa2bb25979eb5be
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-05-13 19:05:15 +0300

    rabbitmq

---
+++ src/utils/rabbitMqReceived.js
@@ -0,0 +1,37 @@
+import amqplib from 'amqplib';
+
+export const receivedMessage = async (queueName) => {
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+        await channel.assertQueue(queueName, { durable: false });
+        console.log(`Waiting for messages in queue: ${queueName}`);
+
+        return new Promise((resolve, reject) => {
+            const messages = [];
+
+            channel.consume(queueName, (msg) => {
+                const messageObject = JSON.parse(msg.content.toString());
+                messages.push(messageObject);
+
+                // Acknowledge the message after processing
+                channel.ack(msg);
+                
+                console.log("Done processing message");
+            }, { noAck: false });
+
+            // Set a longer timeout to wait for messages
+            setTimeout(() => {
+                // Close the channel and connection
+                channel.close();
+                connection.close();
+
+                // Resolve the promise with the collected messages
+                resolve(messages);
+            }, 5000); // Adjust the timeout as needed
+        });
+    } catch (err) {
+        console.error(err);
+        throw new Error(`Error for received Messages: ${err.message}`);
+    }
+};
---
+++ src/utils/rabbitMqSend.js
@@ -0,0 +1,22 @@
+import amqplib from 'amqplib';
+
+export const sendMessage = async (queueName, msg) => {
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+        await channel.assertQueue(queueName, { durable: false });
+
+        const messageString = JSON.stringify(msg);
+        await channel.sendToQueue(queueName, Buffer.from(messageString));
+
+        console.log('sent: ', messageString);
+
+        // Wait for a moment to ensure the message is sent
+        await new Promise(resolve => setTimeout(resolve, 100));
+
+        await connection.close();
+    } catch (err) {
+        console.error(err);
+        throw new Error(`Error for creating sendMessaging: ${err.message}`);
+    }
+}
