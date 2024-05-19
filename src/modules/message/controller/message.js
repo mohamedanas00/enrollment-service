d0ee419c0bb6e2654051192525a66028c90b6528
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-05-19 22:50:42 +0300

    fix bugs

--- src/modules/message/controller/message.js
+++ src/modules/message/controller/message.js
@@ -1,15 +1,14 @@
 import { StatusCodes } from "http-status-codes";
 import messageModel from "../../../../DB/models/message.model.js";
 import { asyncHandler } from "../../../utils/errorHandling.js";
+import { receivedMessage } from "../../../utils/rabbitMqReceived.js";
 
+export const getAllMessages = asyncHandler(async (req, res, next) => {
+  const messages = await messageModel.find({ instructorId: req.user.id });
+  res.status(StatusCodes.OK).json({ messages });
+});
 
-
-export const getAllMessages = asyncHandler(async (req, res,next) => {
-    const messages = await messageModel.find();
-    res.status(StatusCodes.OK).json({ messages });
-})
-
-export const getMessagesByRabbitMQ = asyncHandler(async (req, res,next) => {
-    const messages = await receivedMessage(req.user.email);
-    res.status(StatusCodes.OK).json({ messages });
-})
+export const getMessagesByRabbitMQ = asyncHandler(async (req, res, next) => {
+  const messages = await receivedMessage(req.user.email);
+  res.status(StatusCodes.OK).json({ messages });
+});
